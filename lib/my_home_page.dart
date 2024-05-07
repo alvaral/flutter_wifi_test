6c657d4d09988a62348d0868d8f7f96fc185f032
Alvaro Alonso <alvaro.alonso-perez@comlet.de>
2024-05-07 11:10:50 +0200

    - disconnect wifi before connection - adapt wifi security type - send tcp message (doesnt recieve)

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -4,17 +4,18 @@ import 'dart:developer';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:universal_io/io.dart';
-import 'package:wifi_scan/wifi_scan.dart'; // Importa el paquete wifi_scan
+import 'package:wifi_iot/wifi_iot.dart';
+import 'package:wifi_scan/wifi_scan.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<WiFiAccessPoint> _discoveredNetworks = <WiFiAccessPoint>[];
+  final List<WiFiAccessPoint> _discoveredNetworks = [];
   bool _isScanning = false;
 
   @override
@@ -27,7 +28,7 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _isScanning = true;
     });
-    final can = await WiFiScan.instance.canStartScan();
+    final can = await WiFiScan.instance.canStartScan(askPermissions: true);
 
     if (can != CanStartScan.yes) {
       if (mounted) kShowSnackBar(context, "Cannot start scan: $can");
@@ -46,68 +47,241 @@ class _MyHomePageState extends State<MyHomePage> {
       return;
     }
 
+    Set<String> uniqueSSIDs = {};
+    List<WiFiAccessPoint> filteredNetworks = [];
+
+    for (var network in discoveredNetworks) {
+      if (!uniqueSSIDs.contains(network.ssid)) {
+        uniqueSSIDs.add(network.ssid);
+        filteredNetworks.add(network);
+      }
+    }
+
     setState(() {
       _discoveredNetworks.clear();
-      _discoveredNetworks.addAll(discoveredNetworks);
+      _discoveredNetworks.addAll(filteredNetworks);
       _isScanning = false;
     });
   }
 
-  Future<void> _connectToDevice(WiFiAccessPoint wifiAccessPoint) async {
-    try {
-      // Connect to device using TCP
-      final Socket socket = await Socket.connect(wifiAccessPoint.bssid, 80);
-
-      socket.listen((List<int> event) {
-        // Handle received data
-        final data = utf8.decode(event);
-        log('Received data: $data');
-        // Handle received data accordingly
-      });
-
-      _showWifiService(wifiAccessPoint); // Call to show modal
-    } catch (error) {
-      debugPrint('Error connecting to device: $error');
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Error connecting to device: $error'),
-        ),
-      );
-    }
-  }
-
-  void _showWifiService(WiFiAccessPoint wifiAccessPoint) async {
-    // Show modal with list of services (replace with your desired modal implementation)
+  void _showWifiData(WiFiAccessPoint wifiAccessPoint) async {
+    // Show modal with network details (replace with your desired modal implementation)
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Access Point: ${wifiAccessPoint.ssid}'),
+          title: Text('WiFi Network: ${wifiAccessPoint.ssid}'),
           content: SingleChildScrollView(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ListTile(
-                    title: Text('bssid: ${wifiAccessPoint.bssid}'),
-                    onTap: () => {}),
-                ListTile(
-                    title:
-                        Text('capabilities: ${wifiAccessPoint.capabilities}'),
-                    onTap: () => {}),
-                ListTile(
-                    title: Text('standard: ${wifiAccessPoint.standard}'),
-                    onTap: () => {}),
+                Text('BSSID: ${wifiAccessPoint.bssid}'),
+                Text('Signal Strength: ${wifiAccessPoint.level}'),
+                Text('Frequency: ${wifiAccessPoint.frequency} MHz'),
+                Text('Capabilities: ${wifiAccessPoint.capabilities}'),
               ],
             ),
           ),
           actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text('Close'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    // _showConnectedWifiData(wifiAccessPoint);
+                    _connectToDevice(wifiAccessPoint);
+                  },
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green)),
+                  child: const Text(
+                    'Connect',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Close'),
+                ),
+              ],
             ),
           ],
         );
       },
     );
+  }
+
+  Future<void> _connectToDevice(WiFiAccessPoint wifiNetwork) async {
+    try {
+      await WiFiForIoTPlugin.disconnect();
+
+      NetworkSecurity networkSecurity =
+          getSecurityType(wifiNetwork.capabilities);
+
+      if (!(networkSecurity == NetworkSecurity.WEP ||
+          networkSecurity == NetworkSecurity.WPA)) {
+        await WiFiForIoTPlugin.connect(wifiNetwork.ssid);
+        print('Conectado a ${wifiNetwork.ssid}');
+        _showWifiData(wifiNetwork);
+        return;
+      }
+
+      // WiFi requires password, show modal to input password
+      TextEditingController passwordController = TextEditingController();
+      // ignore: use_build_context_synchronously
+      await showDialog<String?>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Enter the password for ${wifiNetwork.ssid}'),
+            content: TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                hintText: 'Password',
+              ),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () async {
+                  if (passwordController.text != '') {
+                    log('connecting to ${wifiNetwork.ssid} with password ${passwordController.text}');
+
+                    log('capabilities: ${getSecurityType(wifiNetwork.capabilities)}');
+                    await WiFiForIoTPlugin.connect(wifiNetwork.ssid,
+                        bssid: wifiNetwork.bssid,
+                        withInternet: true,
+                        password: passwordController.text,
+                        security: getSecurityType(wifiNetwork.capabilities));
+                    log('Conected to ${wifiNetwork.ssid}');
+                  }
+                  _showConnectedWifiData(wifiNetwork);
+                  // ignore: use_build_context_synchronously
+                  Navigator.pop(context);
+                },
+                child: const Text('Connect'),
+              ),
+            ],
+          );
+        },
+      );
+    } catch (error) {
+      log('Error connecting to wifi: $error');
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error connecting to wifi: $error'),
+        ),
+      );
+    }
+  }
+
+  void _showConnectedWifiData(WiFiAccessPoint wifiAccessPoint) async {
+    String? ipv4Address = await WiFiForIoTPlugin.getIP();
+    Socket? socket;
+
+    try {
+      socket = await Socket.connect('192.168.6.29', 8080);
+      log('Connected to TCP server');
+
+      TextEditingController messageController = TextEditingController();
+      List<String> messages = [];
+
+      // ignore: use_build_context_synchronously
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Connected WiFi Network: ${wifiAccessPoint.ssid}'),
+            content: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('IPv4 Address: $ipv4Address'),
+                  TextField(
+                    controller: messageController,
+                    decoration: const InputDecoration(
+                      labelText: 'Message to send',
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      String message = messageController.text;
+                      if (message.isNotEmpty && socket != null) {
+                        socket.write(message);
+                        messages.add('Sent: $message');
+                        messageController.clear();
+                        setState(
+                            () {}); // Update the UI after sending a message
+                      }
+                    },
+                    child: const Text('Send Message'),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Messages:',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  // ListView.builder(
+                  //   shrinkWrap: true,
+                  //   itemCount: messages.length,
+                  //   itemBuilder: (context, index) {
+                  //     return Text(messages[index]);
+                  //   },
+                  // ),
+                ],
+              ),
+            ),
+            actions: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      if (socket != null) {
+                        socket.close();
+                      }
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Close'),
+                  ),
+                ],
+              ),
+            ],
+          );
+        },
+      );
+
+      // Setup a listener to receive data from the server
+      socket.listen(
+        (List<int> event) {
+          String receivedMessage = utf8.decode(event);
+          messages.add('Received: $receivedMessage');
+          setState(() {}); // Update the UI when receiving a message
+        },
+        onError: (error) {
+          print('Error receiving data: $error');
+        },
+        onDone: () {
+          print('Connection closed');
+        },
+      );
+    } catch (error) {
+      log('Error connecting to TCP server: $error');
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error connecting to TCP server: $error'),
+        ),
+      );
+    }
   }
 
   @override
@@ -131,10 +305,10 @@ class _MyHomePageState extends State<MyHomePage> {
                   itemBuilder: (context, index) {
                     final network = _discoveredNetworks[index];
                     return ListTile(
-                      title: Text(network.ssid),
+                      title: Text(network.ssid.toString()),
                       subtitle: Text('Signal strength: ${network.level} dBm'),
                       onTap: () {
-                        _connectToDevice(_discoveredNetworks[index]);
+                        _showWifiData(network);
                       },
                     );
                   },
@@ -148,4 +322,20 @@ class _MyHomePageState extends State<MyHomePage> {
       ..hideCurrentSnackBar()
       ..showSnackBar(SnackBar(content: Text(message)));
   }
+
+  NetworkSecurity getSecurityType(String text) {
+    if (text.toLowerCase().contains('wpa')) {
+      return NetworkSecurity.WPA;
+    } else if (text.toLowerCase().contains('wep')) {
+      return NetworkSecurity.WEP;
+    } else {
+      return NetworkSecurity.NONE;
+    }
+  }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: MyHomePage(),
+  ));
 }
